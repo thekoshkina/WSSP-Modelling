984d7b68360725a09b1d09f9ef3150efc7cc6b23
Vira Koshkina <vira.koshkina@gmail.com>
2018-05-02 15:18:12 +1000

    select cumberland plain data from bionet and split it into species

--- SDM/transform data.R
+++ SDM/transform data.R
@@ -2,7 +2,7 @@ require(rgdal)
 require(raster)
 require(maptools)
 
-setwd("Data")
+
 # setwd("..")
 
 
@@ -32,13 +32,13 @@ setwd("Data")
 
 
 ## Read files supplied with the project
-Macarthur_Vegetation = readOGR("Data package 20180309/26276_Macarthur_Vegetation_DRAFT_20180223.shp")
-Cadastre = readOGR("Data package 20180309/Cadastre.shp")
-Study_area = readOGR("Data package 20180309/Cumberland_IBRA_subregion.shp")
-Plain_west = readOGR("Data package 20180309/CumberlandPlainWest_2013_E_4207.shp")
-Priority_Growth = readOGR("Data package 20180309/Priority_Growth_Areas.shp")
-Western_Vegetation = readOGR("Data package 20180309/WesternSydneyVeg_prelimDRAFT20180309.shp")
-Wilton_Vegetation = readOGR("Data package 20180309/Wilton_Vegetation_FINAL_20180226.shp", delete_null_obj=TRUE) # the coordinates of this layer don't match the coordinates of the other layers
+Macarthur_Vegetation = readOGR("Data/Data package 20180309/26276_Macarthur_Vegetation_DRAFT_20180223.shp")
+Cadastre = readOGR("Data/Data package 20180309/Cadastre.shp")
+Study_area = readOGR("Data/Data package 20180309/Cumberland_IBRA_subregion.shp")
+Plain_west = readOGR("Data/Data package 20180309/CumberlandPlainWest_2013_E_4207.shp")
+Priority_Growth = readOGR("Data/Data package 20180309/Priority_Growth_Areas.shp")
+Western_Vegetation = readOGR("Data/Data package 20180309/WesternSydneyVeg_prelimDRAFT20180309.shp")
+Wilton_Vegetation = readOGR("Data/Data package 20180309/Wilton_Vegetation_FINAL_20180226.shp", delete_null_obj=TRUE) # the coordinates of this layer don't match the coordinates of the other layers
 
 
 
@@ -52,7 +52,7 @@ Wilton_Vegetation = readOGR("Data package 20180309/Wilton_Vegetation_FINAL_20180
 
 
 
-# Split the files into csv's for each of the species
+# Split the files into csv's for each of the species -----------------------------
 fauna_table=read.csv(file="Threatened_Fauna.csv")
 head(fauna_table[,8:12])
 
@@ -103,3 +103,54 @@ for (i in flora_names)
 	write.csv(eval(parse(text = name)), file=paste("Threatened_Flora_csv/",name,".csv", sep=""))
 
 }
+
+
+require(rgdal)
+
+# bionet_surroundings <- readOGR(dsn = "Data/BionetRecords/26276_Habitat_models.gdb", layer = "All_Bionet_Records_CumberlandPlainIBRA_and_Surrounds")
+# # plot(bionet_surroundings)
+# proj4string(bionet_surroundings)=Study_area@proj4string
+# bionet_cumberland = bionet_surroundings[Study_area, ]
+# write.csv( bionet_cumberland@data, file="Data/bionet_cumberland.csv")
+# add a record of this dataset in the dataset summary file
+# bionet_cumberland_table=read.csv(file="Data/bionet_cumberland.csv")
+# cat( paste ("bionet_cumberland_table","Bionet records that are inside the Cumberland plane IBRA subregion", length(cumberland_names), dim(bionet_cumberland_table)[1],  dim(bionet_cumberland_table)[2], min(as.Date(bionet_cumberland_table$eventDate)),max(as.Date(bionet_cumberland_table$eventDate)), sep=","),file= "Data/data_summary.csv", append=TRUE)
+
+bionet_cumberland_table=read.csv(file="Data/bionet_cumberland.csv")
+# head(bionet_cumberland_table[,1:42])
+
+cumberland_species=NULL
+cumberland_names = unique(bionet_cumberland_table$scientificName)
+write.csv(cumberland_names, file="Data/cumberland_names.csv")
+
+for (i in cumberland_names)
+{
+	tmp=bionet_cumberland_table[bionet_cumberland_table$scientificName==i,]
+
+	name=i
+	name=gsub(" ", "_",name, fixed = TRUE)
+	name=gsub("-", "_",name, fixed = TRUE)
+	name=gsub("/", "_",name, fixed = TRUE)
+	name=gsub(".", "",name, fixed = TRUE)
+	name=gsub("(", "",name, fixed = TRUE)
+	name=gsub(")", "",name, fixed = TRUE)
+	name=gsub("'", "",name, fixed = TRUE)
+	name=gsub("<", "",name, fixed = TRUE)
+
+	cumberland_species=c(cumberland_species, name)
+
+	do.call('=',list(name,tmp))
+
+	write.csv(eval(parse(text = name)), file=paste("Data/Bionet_cumberland/",name,".csv", sep=""))
+
+}
+
+
+
+
+
+# bionet_out_surrounding = bionet_surroundings[!Study_area, ] #select points that are in the geodatabase All_Bionet_Records_CumberlandPlainIBRA_and_Surrounds but not in the area of cubmerland plain IBRA subregion
+
+plot(Study_area)
+plot(bionet_cumberland, add=TRUE)
+
